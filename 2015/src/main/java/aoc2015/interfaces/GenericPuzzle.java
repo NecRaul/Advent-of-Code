569199367fc85abc379d372c3227a5f721827c69
NecRaul <necraul@kuroneko.dev>
2024-01-31 09:03:48 +0400

    AOC 2015 - Template - Java

---
+++ 2015/src/main/java/aoc2015/interfaces/GenericPuzzle.java
@@ -0,0 +1,10 @@
+package aoc2015.interfaces;
+
+import java.util.List;
+
+public interface GenericPuzzle {
+
+    Object solvePart1(List<String> lines);
+
+    Object solvePart2(List<String> lines);
+}
